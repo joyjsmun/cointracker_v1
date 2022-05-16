1b89703e74c6f8454e53022b92a8d6ebd72190e8
joyjsmun <joy.js.mun@gmail.com>
2022-05-16 17:01:23 +0900

    ADD: fetching data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [loading,setLoading] = useState(true);
+  const [coins,setCoins] = useState([])
+  useEffect(() => {
+    (
+      fetch("https://api.coinpaprika.com/v1/tickers?limit=1000")
+    )
+  },[])
+
   return (
    <div>
-     <h1>Coin Tracker</h1>
+     <h1>Coin Tracker()</h1>
+    {loading ? <strong>Loading...</strong> : null}
+
    </div>
   );
 }
