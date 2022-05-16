b2284ea17f50a45f64e5dfb7a7adb031f423cbc5
joyjsmun <joy.js.mun@gmail.com>
2022-05-16 17:18:34 +0900

    ADD : input with amount state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading,setLoading] = useState(true);
   const [coins,setCoins] = useState([])
+  const [amount,setAmount] = useState("")
   useEffect(() => {
       fetch("https://api.coinpaprika.com/v1/tickers?limit=1000")
       .then(response => response.json())
       .then(data => setCoins(data))
       setLoading(false)
   },[])
+
+  const handleDollars = (event) => {
+    setAmount("$ ",event.target.value)
+  }
 
   const coinList = coins.map(coin =>
      <option key={coin.id}>{coin.name} : ${coin.quotes.USD.price}</option>
@@ -17,8 +22,10 @@ function App() {
   return (
    <div>
      <h1>Coin Tracker({loading? null : `${coins.length}`})</h1>
+     <input onChange={handleDollars} type="text" placeholder="How much you have?"/>
     {loading ? <strong>Loading...</strong> : <select>
       {coinList}
+      <hr />
       </select>}
    </div>
   );
